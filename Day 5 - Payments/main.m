75af45833d8af37466f6aa7e340d2ee0df82af20
brokeandfurious <forumbomb@gmail.com>
2017-11-05 23:10:46 -0500

    Initial Commit

---
+++ Day 5 - Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Day 5 - Payments
+//
+//  Created by Murat Ekrem Kolcalar on 11/5/17.
+//  Copyright © 2017 mumu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
